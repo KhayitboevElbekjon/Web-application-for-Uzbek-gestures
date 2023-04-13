ed23fa58745d0a125f4bee6a340cde3a21c60bec
Khayitboev Elbekjon <backenddevolpment@gmail.com>
2023-04-13 21:42:19 +0500

    ushbu loyihaga search qo'shildi

--- static/script.js
+++ static/script.js
@@ -1,12 +1,17 @@
-
 big_infos=document.querySelectorAll(".big_info")
 line_info=document.querySelector(".line_info")
 section_info=document.querySelector(".section_info")
 button_bottom=document.querySelector(".button_bottom")
 bottom_back=document.querySelector(".bottom_back")
 info_text = document.querySelector(".info_text")
+theme_header = document.querySelector(".theme_header")
+header_ul = document.querySelector(".header_ul")
+btn_sreach = document.querySelector(".btn_sreach")
+sreach = document.querySelector(".sreach")
+search_section = document.querySelector(".search_section")
 text=document.querySelector(".text")
 let big_info;
+alert("btn_sreach")
 
 big_infos.forEach(big_info => {
 
@@ -31,16 +36,58 @@ big_infos.forEach(big_info => {
      
       }
       else{
-
         section_info.classList.remove("none")
         line_info.classList.add("none")
+        theme_header.textContent=text.textContent
+        header_ul.classList.add("none")
       }
  })
  
 });
 
+btn_sreach.addEventListener('click',()=>{
+console.log("hi")
+ sreach.classList.remove("none")
+ header_ul.classList.add("none") 
+ line_info.classList.add("none")
+ bottom_back.classList.remove("none")
+ button_bottom.classList.add("none")
+
+})
+
+let filterBigInfos =((text) => {
+
+  let filteredBigInfos = [...big_infos].filter((big_info) => {
+    let bigInfoText = big_info.querySelector(".text").textContent.toLowerCase();
+    return bigInfoText.includes(text);
+  });
+  let lineInfoHTML = "";
+  filteredBigInfos.filter((big_info) => {
+    console.log(big_info);
+    // big_info.addEventListener("click",()=>{
+      //   console.log("dd")
+      // })
+      
+      
+      lineInfoHTML += big_info.outerHTML;
+      
+    line_info.innerHTML = lineInfoHTML;
+    
+    let resultText = search_section.querySelector(".big_info")
+    resultText.innerHTML=lineInfoHTML
+      
+    });
+  })  
+  
+  sreach.addEventListener("input", (e) => {
+    let value = e.target.value.toLowerCase();
+    filterBigInfos(value);
+    search_section.classList.remove("none")  ;
+  theme_header.textContent=value
+});
 
 bottom_back.addEventListener('click',()=>{
   location.reload()
-  
-})
+
+}
+)
